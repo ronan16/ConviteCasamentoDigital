cb467deb2e151f97c9e841fcd7cfeb0e5fee0a62
Ronan Lopes <ronan.anacletolopes@gmail.com>
2022-01-22 18:25:02 -0300

    Atualização do estilo da pagina

--- Casamento_files/form_whatsapp.js
+++ Casamento_files/form_whatsapp.js
@@ -8,7 +8,6 @@ document.querySelector('#confirmar_presenca').addEventListener('click',function(
 let url = ("https://api.whatsapp.com/send?phone=5543999799500&text= Estou confirmando minha presença no Casamento. Dia 19/03/2022 às 17:00. %0A Meu nome: %0A " + 
 			nome + " %0A Vou levar " + convidado + " pessoa(s) comigo. %0A Meu telefone pra contato é: "+ fone +" %0A Meu e-mail é: "+ email +
 			" %0A Quero deixar um recado especial:  %0A" + mensagem );
-alert(url);
 window.open(url);
 
 });
